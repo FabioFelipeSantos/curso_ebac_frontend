1222934c271f922e2479c7b4c142f24e7deddfd0
FabioTestGit <fabiotrabmat@gmail.com>
2024-08-05 14:55:23 -0300

    chore: Remove unused code files and update canvas dimensions

--- modulo_5-projeto/scripts/main.js
+++ modulo_5-projeto/scripts/main.js
@@ -7,23 +7,30 @@ let ctx = canvas.getContext("2d");
 let object;
 
 function setCanvasSizeAndHeaderHeight() {
-  const elementWidth = +getComputedStyle(section).width.slice(0, -2) * 0.6;
-  const elementHeight = elementWidth * goldenRatio;
-  section.style.height = `${elementHeight}px`;
-  canvas.width = elementWidth;
-  canvas.height = elementHeight;
+	section.style.height = `fit-content`;
+	const sectionSizes = section.getBoundingClientRect();
+	let elementHeight = sectionSizes.height;
+	let elementWidth = elementHeight / goldenRatio;
+
+	if (elementWidth > sectionSizes.width) {
+		canvas.style.right = `${-(elementWidth - sectionSizes.width) / 2}px`;
+	}
+
+	canvas.width = elementWidth;
+	canvas.height = elementHeight;
+	// console.log(elementWidth, elementHeight);
 }
 
 window.onload = () => {
-  setCanvasSizeAndHeaderHeight()
-  object = new CanvasObject(ctx, canvas.width, canvas.height);
-  object.drawStrokeRectangle();
-  object.drawFibonacciLines(20);
+	setCanvasSizeAndHeaderHeight()
+	object = new CanvasObject(ctx, canvas.width, canvas.height);
+	object.drawStrokeRectangle();
+	object.drawFibonacciLines(20);
 }
 
 window.addEventListener("resize", () => {
-  setCanvasSizeAndHeaderHeight()
-  object = new CanvasObject(ctx, canvas.width, canvas.height);
-  object.drawStrokeRectangle();
-  object.drawFibonacciLines(20);
+	setCanvasSizeAndHeaderHeight()
+	object = new CanvasObject(ctx, canvas.width, canvas.height);
+	object.drawStrokeRectangle();
+	object.drawFibonacciLines(20);
 })
