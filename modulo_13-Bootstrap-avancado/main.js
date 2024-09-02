ad2433c20e36586181d1f88cf9b520305490105a
FabioTestGit <fabiotrabmat@gmail.com>
2024-09-02 12:45:49 -0300

    finalização do projeto

--- modulo_13-Bootstrap-avancado/main.js
+++ modulo_13-Bootstrap-avancado/main.js
@@ -3,10 +3,11 @@ const tooltipList = [...tooltipTriggerList].map(
 	(tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
 );
 
-$("#table-metal-themes").DataTable({
-	searching: false,
-	layout: {
-		bottomStart: null,
-		bottomEnd: null
-	}
-});
+const date = new Date();
+
+$("#footer-date-time").attr(
+	"datetime",
+	[date.getUTCFullYear(), date.getUTCMonth()+1 < 10 ? `0${date.getUTCMonth()+1}` : date.getUTCMonth() + 1, date.getUTCDate() < 10 ? `0${date.getUTCDate()}` : date.getUTCDate()].join("-")
+);
+
+$("#footer-date-time").html(date.getUTCFullYear());
