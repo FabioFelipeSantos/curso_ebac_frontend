39a4568054769ed5e8b2f9ac6261afb3d7d8cb97
Fábio Santos <fabiotrabmat@gmail.com>
2024-09-30 20:18:15 -0300

    chore: terminando o projeto

--- modulo_21-projeto-4/clone-disney-plus/gulpfile.js
+++ modulo_21-projeto-4/clone-disney-plus/gulpfile.js
@@ -2,6 +2,11 @@ const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
 const sourcemaps = require("gulp-sourcemaps");
+const uglify = require("gulp-uglify");
+
+function scripts() {
+	return gulp.src("./src/scripts/*.js").pipe(uglify()).pipe(gulp.dest("./dist/js"));
+}
 
 function styles() {
 	return gulp
@@ -19,7 +24,8 @@ function images() {
 		.pipe(gulp.dest("./dist/img"));
 }
 
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 exports.watch = function () {
 	gulp.watch("./src/styles/**/*.sass", gulp.parallel(styles));
+	gulp.watch("./src/scripts/**/*.js", gulp.parallel(scripts));
 };
